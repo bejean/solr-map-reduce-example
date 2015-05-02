45e8c2bba7183b94c6bee8aa0117d72ab0d2d75f
Dominique Béjean <dominique.bejean@eolya.fr>
2015-05-02 17:10:34 +0200

    no message

--- scripts/setup-example-test.sh
+++ scripts/setup-example-test.sh
@@ -6,4 +6,8 @@ export HOME="$( cd "$( dirname "$0" )/.." && pwd )"
 . $HOME/scripts/setenv.sh
 cd $HADOOP_HOME
 
+bin/hdfs dfs -mkdir /user
+bin/hdfs dfs -mkdir /user/root
+bin/hdfs dfs -put etc/hadoop input
+
 bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar grep input output 'dfs[a-z.]+'
